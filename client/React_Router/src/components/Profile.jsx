4815f6e9687c91b722c915383b1c0f712a0a2017
devsalman247 <salman247.dev@gmail.com>
2022-09-28 18:52:16 +0500

    some changes

---
+++ client/React_Router/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Profile() {
+    return (
+        <div>
+            <h1 className='capitalize mt-4 mb-4'>You are inside nested profile page</h1>
+        </div>
+    )
+}
+
+export default Profile
---
+++ client/React_Router/src/components/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function User() {
+    return (
+        <div>
+            <h1 className='capitalize mt-4 mb-4'>You are inside nested user page</h1>
+        </div>
+    )
+}
+
+export default User
